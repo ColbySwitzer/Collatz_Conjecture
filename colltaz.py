7792a7d9f39e49556293725d85e8179ee0f99036
Colby Switzer <colbyswitzer3@gmail.com>
2023-12-09 22:19:29 -0500

    Created matplotlib function to create a scatterplot graph

--- colltaz.py
+++ colltaz.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 currentNum = 3
 running = False
 
-def onButtonClick():
-    running = not running
 
 def onClosing():
     window.destroy()
@@ -33,12 +31,42 @@ def runCollatzConjecture():
             print(collatzConjecture(currentNum,"p"))
             currentNum = currentNum + 1
 
+def onButtonClick():
+    # running = not running
+    x = [1, 2, 3, 4, 5]
+    y = [2, 3, 1, 4, 5]
+    updatePlot(x,y)
+
+
+def updatePlot(x,y):
+    ax.clear()
+
+    ax.scatter(x,y, label='Scatter Plot',color='blue',marker='o')
+
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_title('Scatter Plot Example')
+
+    canvas.draw()
+
 window = tk.Tk()
 window.title("Collatz Conjecture")
 window.geometry("1200x675")
 
-label = tk.Label(window, text="Text Field")
+label = tk.Label(window, text="Enter your name:")
 label.pack(pady = 10)
+
+entry = tk.Entry(window)
+entry.pack(pady=10)
+
+button = tk.Button(window, text="Click me", command=onButtonClick)
+button.pack(pady=10)
+
+fig, ax = plt.subplots()
+
+canvas = FigureCanvasTkAgg(fig, master=window)
+canvas_widget = canvas.get_tk_widget()
+canvas_widget.pack()
 
 # window.protocol("VM_DELETE_WINDOW", onClosing)
 
